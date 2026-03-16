07c1b9f1c0dff2bf27ad4399ff686596b5e44e58
Angel <angelecheverriah@gmail.com>
2026-03-16 12:59:25 -0400

    modificando archivo vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    // Esto permite que Railway (o cualquier host) sirva la app
+    allowedHosts: [
+      "frontend-production-f56d.up.railway.app", // Tu URL específica
+      ".up.railway.app", // Opcional: permite cualquier subdominio de Railway
+    ],
+  },
+});
